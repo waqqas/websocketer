92be2131f4e0c1bb7e53702e9a2a6fc8cd0718f6
Waqqas Jabbar <waqqas.jabbar@gmail.com>
2021-02-04 10:52:56 +0500

    Updated session test

--- tests/session_tests.cpp
+++ tests/session_tests.cpp
@@ -5,6 +5,8 @@
 
 TEST_CASE("session")
 {
+  namespace beast = boost::beast;
+
   bool passed = false;
 
   std::string host("echo.websocket.org");
@@ -12,14 +14,26 @@ TEST_CASE("session")
 
   boost::asio::io_context    io;
   websocketer::asio::session session(io);
+  beast::flat_buffer         buffer;
+  std::string                data_to_send("hello world");
 
   session.async_open(host, service, [&](const boost::system::error_code &ec) {
     if (!ec)
     {
-      session.async_close([&](const boost::system::error_code &ec) {
+      session.async_write(data_to_send, [&](const boost::system::error_code &ec, std::size_t) {
         if (!ec)
         {
-          passed = true;
+          session.async_read(buffer, [&](const boost::system::error_code &ec, std::size_t) {
+            if (!ec)
+            {
+              session.async_close([&](const boost::system::error_code &ec) {
+                if (!ec)
+                {
+                  passed = true;
+                }
+              });
+            }
+          });
         }
       });
     }
